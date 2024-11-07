f727729f25c83c7dcf4a4f84a81eefb636f18896
Akshat Jaimini <destrex271@gmail.com>
2024-11-07 21:43:58 +0530

    updated notif

--- backend/index.js
+++ backend/index.js
@@ -404,7 +404,7 @@ app.post("/addedSlaveDevice", authenticateToken, async(req, res) => {
 
 
 app.post("/send_notif", async(req, res) => {
-  sendNotification(req.body.userId, {msg: "Hello", body: "Hello !"})
+  sendNotification(req.body.userId, "Title", "Hello")
   res.sendStatus(200)
 })
 
